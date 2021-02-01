3d46d33b196d340bae0886c4bd5ef603e774c3b8
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-02-01 08:09:04 +0300

    add Form Material UI

--- src/frontend/components/feedback/feedbackForm/feedbackForm.jsx
+++ src/frontend/components/feedback/feedbackForm/feedbackForm.jsx
@@ -3,41 +3,63 @@ import {connect} from 'react-redux'
 import styles from './feedbackForm.module.scss'
 import useForm from "../../../hooks/use-form"
 import {addFeedback} from "../../../redux/actions/action";
+import { makeStyles } from '@material-ui/core/styles';
+import Input from "../../controls/input";
+import Button from "../../controls/button";
 
-const INITIAL_VALUES = {name: '', text: '', phone: ''}
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        },
+    },
+}));
 
 const FeedbackForm = (props) => {
     console.log('[FeedbackForm][props]', props)
+    const INITIAL_VALUES = {name: '', lastname: '', phone: ''}
     const {onSubmit} = props
-    const {values, handlers, reset} = useForm(INITIAL_VALUES)
+
+    const validate = (fieldValues = values) => {
+        let temp = {...errors}
+        if('name' in fieldValues) {
+            temp.name = fieldValues.name ? "" : "This field is required"
+        }
+        // if('lastname' in fieldValues) {
+        //     temp.lastname = fieldValues.lastname ? "" : "This field is required"
+        // }
+        // if('phone' in fieldValues) {
+        //     temp.phone = fieldValues.phone.length > 11 ? "" : "Minimum 11 numbers required"
+        // }
+        setErrors({...temp})
+
+        if(fieldValues === values) {
+            return Object.values(temp).every(x => x === '')
+        }
+    }
+
+    const {values, setValues, errors, setErrors, handleInputChange, resetForm} = useForm(INITIAL_VALUES, true, validate)
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        onSubmit(values)
-        reset()
+        if (validate()) {
+            console.log('OK')
+            onSubmit(values)
+            resetForm()
+        }
     }
+
+
+    const classes = useStyles();
 
     return (
         <div className={styles.feedbackForm}>
             <h2 className={styles.title}>HEADER</h2>
             <div className={styles.form}>
-                <form action="POST" onSubmit={handleSubmit}>
-                    <input
-                        placeholder="Name"
-                        className={styles.message}
-                        {...handlers.name}
-                    />
-                    <input
-                        placeholder="Last Name"
-                        className={styles.message}
-                        {...handlers.text}
-                    />
-                    <input
-                        placeholder="Phone"
-                        className={styles.message}
-                        {...handlers.phone}
-                    />
-                    <button>Submit</button>
+                <form className={classes.root} noValidate autoComplete="off" action="POST" onSubmit={handleSubmit}>
+                    <Input name="name" label="Name" value={values.name} onChange={handleInputChange} error={errors.name} />
+                    <Button type="submit" text="Submit" />
                 </form>
             </div>
         </div>
--- src/frontend/hooks/use-form.js
+++ src/frontend/hooks/use-form.js
@@ -1,24 +1,59 @@
 import {useState} from 'react'
 
-const useForm = (initialValues = {}) => {
+const useForm = (initialValues, validateOnChange= false, validate) => {
     const [values, setValues] = useState(initialValues)
+    const [errors, setErrors] = useState({})
 
-    const setValue = (key, value) => setValues({...values, [key]: value})
+    const handleInputChange = e => {
+        const {name, value} = e.target
+        setValues({
+            ...values,
+            [name]: value
+        })
 
-    const onChange = (key) => (ev) =>
-        setValue(key, ev.target ? ev.target.value : ev)
+        if(validateOnChange) {
+            validate({ [name]: value })
+        }
+    }
+
+    const resetForm = () => {
+        setValues(initialValues)
+        setErrors({})
+    }
 
     return {
         values,
-        reset: () => setValues(initialValues),
-        handlers: Object.keys(values).reduce(
-            (acc, key) => ({
-                ...acc,
-                [key]: {value: values[key], onChange: onChange(key)},
-            }),
-            {}
-        ),
+        setValues,
+        errors,
+        setErrors,
+        handleInputChange,
+        resetForm
     }
 }
 
 export default useForm
+
+
+
+// const useForm = (initialValues = {}) => {
+//     const [values, setValues] = useState(initialValues)
+//
+//     const setValue = (key, value) => setValues({...values, [key]: value})
+//
+//     const onChange = (key) => (ev) =>
+//         setValue(key, ev.target ? ev.target.value : ev)
+//
+//     return {
+//         values,
+//         reset: () => setValues(initialValues),
+//         handlers: Object.keys(values).reduce(
+//             (acc, key) => ({
+//                 ...acc,
+//                 [key]: {value: values[key], onChange: onChange(key)},
+//             }),
+//             {}
+//         ),
+//     }
+// }
+//
+// export default useForm
