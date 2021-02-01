9121f24c7cc61ba78affe48adc78b7766f669f0d
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-02-01 08:14:51 +0300

    fix not used vars

--- src/frontend/hooks/use-form.js
+++ src/frontend/hooks/use-form.js
@@ -35,25 +35,3 @@ export default useForm
 
 
 
-// const useForm = (initialValues = {}) => {
-//     const [values, setValues] = useState(initialValues)
-//
-//     const setValue = (key, value) => setValues({...values, [key]: value})
-//
-//     const onChange = (key) => (ev) =>
-//         setValue(key, ev.target ? ev.target.value : ev)
-//
-//     return {
-//         values,
-//         reset: () => setValues(initialValues),
-//         handlers: Object.keys(values).reduce(
-//             (acc, key) => ({
-//                 ...acc,
-//                 [key]: {value: values[key], onChange: onChange(key)},
-//             }),
-//             {}
-//         ),
-//     }
-// }
-//
-// export default useForm
